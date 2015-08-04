6ffca0a608d503c4870c86a4a40967ab7242db88
Kagami Hiiragi <kagami@genshiken.org>
2015-08-04 16:22:06 +0300

    Optimized build

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,38 @@
 import path from "path";
+import webpack from "webpack";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 function insrc(...parts) {
   return new RegExp("^" + path.join(__dirname, "src", ...parts) + "$");
 }
+
+const DEBUG = process.env.NODE_ENV !== "production";
+const JS_NAME = DEBUG ? "webm.js" : "[chunkhash:10].webm.js";
+const HTML_MINIFIER = DEBUG ? false : {
+  removeComments: true,
+  collapseWhitespace: true,
+  minifyCSS: true,
+};
+const COMMON_PLUGINS = [
+  new HtmlWebpackPlugin({
+    minify: HTML_MINIFIER,
+    template: path.join("src", "index", "index.html"),
+    getNameBySuffix: function(stats, suffix) {
+      return stats.assets.find(
+        asset =>
+          asset.name === suffix ||
+          asset.name.slice(-suffix.length - 1) === "." + suffix
+      ).name;
+    },
+  }),
+];
+const PLUGINS = DEBUG ? COMMON_PLUGINS : COMMON_PLUGINS.concat([
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    output: {comments: false},
+    compress: {warnings: false},
+  }),
+]);
 
 // TODO(Kagami): Minify JS and HTML in production mode, use hashed
 // names.
@@ -11,23 +40,12 @@ export default {
   entry: "./src/index",
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "webm.js",
+    filename: JS_NAME,
   },
   module: {
     loaders: [
       {test: insrc(".+\\.js"), loader: "babel"},
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join("src", "index", "index.html"),
-      getNameBySuffix: function(stats, suffix) {
-        return stats.assets.find(
-          asset =>
-            asset.name === suffix ||
-            asset.name.slice(-suffix.length - 1) === "." + suffix
-        ).name;
-      },
-    }),
-  ],
+  plugins: PLUGINS,
 };
