d87b3f575f447a638dddf1d9f36011c5109300a2
Kagami Hiiragi <kagami@genshiken.org>
2015-08-05 18:14:38 +0300

    Use Blob url for sample
    Because browser doesn't always cache it once it was loaded via XHR for
    some reason. Since we store response in memory anyway, let's reuse it
    for URL. Hope it won't result in extra memory consumption.

--- src/source/index.js
+++ src/source/index.js
@@ -58,7 +58,9 @@ export default React.createClass({
       if (req.status >= 200 && req.status < 400) {
         const name = SAMPLE_NAME;
         const data = req.response;
-        this.props.onLoad({name, data, url: SAMPLE_URL});
+        const blob = new Blob([data]);
+        const url = URL.createObjectURL(blob);
+        this.props.onLoad({name, data, url});
       } else {
         // FIXME(Kagami): Display error in UI.
         this.setState({loadingSample: false});
